86915061aeed9daccb7b05db1337f6e0d9393443
Clark Zhang <czhang0312@gmail.com>
2026-05-31 12:05:47 -0400

    make overflow white above navheader

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>
-          {children}
+          <div className="min-h-screen bg-neutral-100">
+            {children}
+          </div>
         </AuthProvider>
       </body>
     </html>
