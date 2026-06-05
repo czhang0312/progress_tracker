c278807825b677f454038aad7bfda5ac1ef9bfb5
Clark Zhang <czhang0312@gmail.com>
2026-06-05 15:20:27 -0400

    remove session debug logging from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,21 +4,9 @@ class ApplicationController < ActionController::Base
 
   # Require authentication by default, but allow selected read-only guest routes.
   before_action :authenticate_user!, unless: :allow_guest_access?
-  before_action :log_session_debug
 
   # Add helper methods for views
   helper_method :current_user, :guest_user?
-
-  def log_session_debug
-    Rails.logger.info "=== Request Debug ==="
-    Rails.logger.info "Path: #{request.path}"
-    Rails.logger.info "user_signed_in?: #{user_signed_in?}"
-    Rails.logger.info "current_user: #{current_user&.email}"
-    Rails.logger.info "session.id: #{session.id}"
-    Rails.logger.info "cookies present: #{request.cookies.keys}"
-    Rails.logger.info "session cookie: #{request.cookies['_progress_tracker_session']&.first(50)}"
-    Rails.logger.info "===================="
-  end
 
   def guest_user?
     !user_signed_in?
